4ad834f6ea269b6ddfd017204b1d676f7126b46f
roshan-2501 <roshant2513@gmail.com>
2025-08-17 15:14:06 +0530

    Synchronization of Keycloak

---
+++ gateway/src/main/java/com/fitness/gateway/user/RegisterRequest.java
@@ -0,0 +1,23 @@
+package com.fitness.gateway.user;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class RegisterRequest {
+
+    @NotNull(message = "Email cannot be empty")
+    @Email(message = "Invalid email format")
+    private String email;
+    private String keyCloakId;  
+    @NotNull(message="Password is Required")
+    @Size(min=6, message = "Password must be atleast of 6 characters")
+    private String password;
+    private String firstName;
+    private String lastName;
+
+
+
+}
--- userservice/src/main/java/com/fitness/userservice/service/UserService.java
+++ userservice/src/main/java/com/fitness/userservice/service/UserService.java
@@ -36,6 +36,7 @@ public class UserService {
         User user=new User();
         user.setEmail(request.getEmail());
         user.setPassword(request.getPassword());
+        user.setKeyCloakId(request.getKeyCloakId());
         user.setFirstName(request.getFirstName());
         user.setLastName(request.getLastName());
 
