3601e03736bb02b6f04b47085c7f6048d7c40d62
roshan-2501 <roshant2513@gmail.com>
2025-08-19 16:50:46 +0530

    Building PKCE Authorization Flow

--- fitness-frontend/src/App.jsx
+++ fitness-frontend/src/App.jsx
@@ -1,13 +1,38 @@
+import React, { useContext, useEffect, useState } from "react";
 import { Button } from "@mui/material";
+import { AuthContext } from "react-oauth2-code-pkce";
 import { BrowserRouter as Router, Navigate, Route, Routes, useLocation } from "react-router";
+import { useDispatch } from "react-redux";
+import { setCredentials } from "./store/authSlice";
 
 function App() {
 
+  const { token, tokenData, logIn, logOut, isAuthenticated } = useContext(AuthContext);
+  const dispatch = useDispatch();
+  const [authReady, setAuthReady] = useState(false);
+
+  useEffect(() => {
+    if (token) {
+      dispatch(setCredentials({ token, user: tokenData }));
+      setAuthReady(true);
+    }
+  }, [token, tokenData, dispatch]);
+
+
   return (
     <Router>
-      <Button variant="contained" color="primary">
-        Login
-      </Button>
+      {!token ? (
+          <Button variant="contained" color="primary" onClick={() => {
+                  logIn();
+                }}>
+                  Login
+          </Button>
+      ) : (
+        <div>
+          <pre>{JSON.stringify(tokenData, null, 2)}</pre>
+        </div>
+      )}
+      
     </Router>
   )
 }
